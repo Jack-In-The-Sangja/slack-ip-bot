c0022f4645153e169e442b7e60e9897c71a14a6f
Shinhuiwon <gump71036969@gmail.com>
2025-07-19 18:43:06 +0900

    feat: slack_bot app create + url setting
    기본 앱 생성 및 URL 테스트

--- setting/urls.py
+++ setting/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),  # '' 경로에 myapp의 urls를 연결
 ]
