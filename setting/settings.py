c9576f518a74efc8ca27757f2f16d93d5b98db8b
Shinhuiwon <gump71036969@gmail.com>
2025-07-19 19:01:47 +0900

    fix: settings.py fix
    settings 수정

--- setting/settings.py
+++ setting/settings.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # .env 파일 경로 지정 및 로드
-load_dotenv(BASE_DIR / '.env')
+load_dotenv(str(BASE_DIR / '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
