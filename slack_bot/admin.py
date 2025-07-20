994f12db7c97f65be632c8a72b5d55523c110c37
Shinhuiwon <gump71036969@gmail.com>
2025-07-20 13:12:19 +0900

    Initial commit: Django project setup

--- slack_bot/admin.py
+++ slack_bot/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import IPAccessRequest
 
-# Register your models here.
+@admin.register(IPAccessRequest)
+class IPAccessRequestAdmin(admin.ModelAdmin):
+    list_display = ('ip_address', 'list_type', 'type', 'device', 'requester', 'requested_at', 'expires_at')
+    search_fields = ('ip_address', 'requester')
+    list_filter = ('list_type', 'device', 'type')
+    ordering = ('-requested_at',)
--- slack_bot/models.py
+++ slack_bot/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class IPAccessRequest(models.Model):
+    list_type = models.CharField(max_length=20)  # 'whitelist' or 'blacklist'
+    type = models.CharField(max_length=20)  # 요청 형식 (e.g. add, remove)
+    device = models.CharField(max_length=50)  # 적용 대상 기기
+    ip_address = models.GenericIPAddressField()  # 요청 IP
+    requester = models.CharField(max_length=100)  # 요청자 식별자
+    requested_at = models.DateTimeField(auto_now_add=True)  # 요청 시간
+    expires_at = models.DateTimeField()  # 만료 시간
+
+    def __str__(self):
+        return f"[{self.list_type.upper()}] {self.type} - {self.ip_address} ({self.requester})"
+
+    class Meta:
+        verbose_name = "IP Access Request"
+        verbose_name_plural = "IP Access Requests"
+        ordering = ['-requested_at']
