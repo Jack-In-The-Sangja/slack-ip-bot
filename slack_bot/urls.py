bcc17e6336d85aa1a3d598011cd358176ddedfb3
Shinhuiwon <gump71036969@gmail.com>
2025-07-22 18:09:08 +0900

    fix: url path
    - url path 수정

--- slack_bot/urls.py
+++ slack_bot/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import security_group_dashboard_view
 
 urlpatterns = [
-    path('security-groups/dashboard/', security_group_dashboard_view, name='security-group-dashboard'),
+    path('', security_group_dashboard_view, name='security-group-dashboard'),
 ]
